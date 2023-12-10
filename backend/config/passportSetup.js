c13959611c2148159ba0153ab9d771d4a0f54a93
mshreeharsha <shreeharsha.mbhat@gmail.com>
2023-12-10 13:31:32 +0530

    Added JWT Token and Passport Callback Function

--- backend/config/passportSetup.js
+++ backend/config/passportSetup.js
@@ -1,6 +1,25 @@
 const passport=require('passport')
 const GoogleStatergy = require('passport-google-oauth20')
 const keys=require('./keys')
+const User=require('../models/userModel')
+const jwt=require('jsonwebtoken')
+
+//Function to generate jwt
+const createToken=(_id)=>{
+    return jwt.sign({_id},keys.token.secretKey,{expiresIn: '1d'});
+}
+
+passport.serializeUser((user,done)=>{
+    done(null,user.user._id)
+})
+
+passport.deserializeUser((id,done)=>{
+    User.findById(id).then((user)=>{
+        if(user){
+            done(null,user)
+        }
+    })
+})
 
 passport.use(new GoogleStatergy({
         clientID:keys.google.clientID,
@@ -8,5 +27,24 @@ passport.use(new GoogleStatergy({
         callbackURL:'/auth/google/redirect'
     },(accessToken,refreshToken,profile,done)=>{
         console.log(profile)
+
+        User.findOne({googleID:profile.id}).then((user)=>{
+            if(user){
+                //User already Exist
+                const token=createToken(user._id)
+                done(null,{user,token})
+            }
+            else{
+                new User({
+                    username:profile.displayName,
+                    googleID:profile.id
+                }).save()
+                .then((newUser)=>{
+                    console.log(`New User Created : ${newUser}`)
+                    const token=createToken(newUser._id)
+                    done(null,{newUser,token})
+                })
+            }
+        })
     }
 ))
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,8 +5,15 @@ const router=express.Router()
 router.get('/google',passport.authenticate('google',{
     scope:["profile"]
 }))
-router.get('/google/redirect',passport.authenticate('google',{
-    successRedirect:'http://localhost:3000/',
-    failureRedirect:'http://localhost:3000/login'
-}))
+
+router.get('/google/redirect',passport.authenticate('google'),
+(req,res)=>{
+    res.send({
+        success:true,
+        message:'User Logged in Successfully',
+        user:req.user.user,
+        token:req.user.token
+    })
+})
+
 module.exports=router
